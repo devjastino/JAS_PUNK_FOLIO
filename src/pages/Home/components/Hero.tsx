46498baa05fb93f218cef5a3ddb872ff74ede112
devjastino <dev.justine.sioson.juan@gmailc.om>
2025-03-31 15:07:29 +0800

    Updated

--- src/pages/Home/components/Hero.tsx
+++ src/pages/Home/components/Hero.tsx
@@ -1,9 +1,19 @@
-import React from "react";
-
 const Hero = () => {
-  return <main className="hero">
-    <h1>Lorem Ipsum</h1>
-  </main>;
+  return (
+    <main className="hero">
+      <div className="flex-center text-center max-w-7xl mx-auto">
+        <h1 className="text-9xl font-title">Lorem Ipsum</h1>
+        <p className="text-2xl font-display">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur.
+        </p>
+      </div>
+    </main>
+  );
 };
 
 export default Hero;
