4db0151085fde1574759432880eb160c3a111912
devjastino <dev.justine.sioson.juan@gmailc.om>
2025-04-01 16:21:45 +0800

    Updated

--- src/pages/Home/components/Hero.tsx
+++ src/pages/Home/components/Hero.tsx
@@ -2,14 +2,12 @@ const Hero = () => {
   return (
     <main className="hero">
       <div className="flex-center text-center max-w-7xl mx-auto">
-        <h1 className="text-9xl font-title">Lorem Ipsum</h1>
+        <h1 className="text-9xl font-title">JAS: Code Anarchy in Motion</h1>
         <p className="text-2xl font-display">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur.
+          Welcome to JAS, where code meets chaos and creativity knows no limits.
+          This isn’t just development—it’s a rebellion. I craft digital
+          experiences that defy expectations, blending tech, art, and raw
+          innovation into something untamed.
         </p>
       </div>
     </main>
