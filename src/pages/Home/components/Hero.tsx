f177b16643ba8150c686e876ea437530f3624efa
devjastino <dev.justine.sioson.juan@gmailc.om>
2025-04-01 17:39:11 +0800

    Updated

--- src/pages/Home/components/Hero.tsx
+++ src/pages/Home/components/Hero.tsx
@@ -1,14 +1,23 @@
+import { FaArrowCircleRight } from "react-icons/fa";
 const Hero = () => {
   return (
     <main className="hero">
       <div className="flex-center text-center max-w-7xl mx-auto">
-        <h1 className="text-9xl font-title">JAS: Code Anarchy in Motion</h1>
-        <p className="text-2xl font-display">
+        <h1 className="text-9xl font-title leading-[0.8] tracking-tight text-zinc-50">
+          Code Anarchy in Motion
+        </h1>
+        <p className="text-2xl font-display mb-6">
           Welcome to JAS, where code meets chaos and creativity knows no limits.
           This isn’t just development—it’s a rebellion. I craft digital
           experiences that defy expectations, blending tech, art, and raw
           innovation into something untamed.
         </p>
+        <button type="button" className="btn-lm">
+          Learn More
+          <span>
+            <FaArrowCircleRight />
+          </span>
+        </button>
       </div>
     </main>
   );
